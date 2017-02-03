c17029486d11b82f42412abb09d1890e7be1ecb8
Tatsiana Zare <danee4ka2@mail.ru>
2017-02-04 01:06:07 +0300

    Add the rest of functions

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,6 +1,7 @@
 class TicTacToe {
     constructor() {
         this.currentPlayer = 'x';
+        this.winner = null;
         this.matrix = [[null, null, null],
                        [null, null, null],
                        [null, null, null]];
@@ -15,6 +16,55 @@ class TicTacToe {
     nextTurn(rowIndex, columnIndex) {
         if (this.matrix[rowIndex][columnIndex] == null) {
             this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+
+            var winnerFounded = false;
+
+            // Check rows
+            for (var i = 0; i < 3; i++) {
+                var count = 0;
+                for (var j = 0; j < 3; j++) {
+                    if (this.matrix[i][j] == this.currentPlayer)
+                        count++;
+                }
+                if (count == 3) {
+                    winnerFounded = true;
+                    break;
+                }
+            }
+
+            // Check columns
+            for (var i = 0; i < 3; i++) {
+                var count = 0;
+                for (var j = 0; j < 3; j++) {
+                    if (this.matrix[j][i] == this.currentPlayer)
+                        count++;
+                }
+                if (count == 3) {
+                    winnerFounded = true;
+                    break;
+                }
+            }
+            
+            // Check diagonals
+            var count1 = 0;
+            var count2 = 0;
+            for (var i = 0; i < 3; i++) {
+                
+                if (this.matrix[i][i] == this.currentPlayer) {
+                    count1++;
+                }
+                if (this.matrix[i][2 - i] == this.currentPlayer) {
+                    count2++;
+                }
+            }
+            if (count1 == 3 || count2 == 3) {
+                winnerFounded = true;
+            }
+
+            if (winnerFounded) {
+                this.winner = this.currentPlayer;
+            }
+
             if (this.currentPlayer == 'x') {
                 this.currentPlayer = 'o';
             } else {
@@ -24,40 +74,34 @@ class TicTacToe {
     }
 
     isFinished() {
-       // if (this.isDraw() == true || this.getWinner() !== null)
 
+       return this.isDraw() || this.getWinner() !== null;
     }
 
     getWinner() {
-        if (this.isDraw() == true) {
 
-            return null;
-        } 
-        else
-        {
-
-        }
-
+        return this.winner;
     }
 
     noMoreTurns() {
+        var result = true;
         for (var i = 0; i < 3; i++) {
             for (var j = 0; j < 3; j++) {
                 if (this.matrix[i][j] == null) {
 
-                    return false;
+                    result = false;
                 }
 
             }
         }
          
-        return true;
+        return result;
     }
 
     isDraw() {
 
         return this.noMoreTurns() && this.getWinner() == null;
-     }
+    }
 
     getFieldValue(rowIndex, colIndex) {
 
